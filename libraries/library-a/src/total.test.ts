6372d35c79f62be8ae76b39b9df32850b0d3f6cf
Christian Micle <christian.micle@pointclickcare.com>
2024-06-19 12:35:04 -0400

    clean up

--- libraries/library-a/src/total.test.ts
+++ libraries/library-a/src/total.test.ts
@@ -1,7 +1,7 @@
 import total from './total';
 
 describe('LibA', () => {
-    it('total is 3 3', () => {
+    test('total is 3 3', () => {
         expect(total("Total", 1, 2)).toBe('Total: 3');
     });
 });
--- libraries/library-b/src/index.test.ts
+++ libraries/library-b/src/index.test.ts
@@ -1,7 +1,7 @@
 import sum from './index';
 
 describe('LibB', () => {
-    it('sum 1 + 2 = 3', () => {
+    test('sum 1 + 2 = 3', () => {
         expect(sum(1, 2)).toBe(3);
     });
 });
